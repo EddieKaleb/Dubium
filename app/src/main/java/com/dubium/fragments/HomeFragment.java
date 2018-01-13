3bd3b6b19e2de656f4c374320f48114dc50fa604
Mateus de Lima <mateus.oliveira@ccc.ufcg.edu.br>
2018-01-13 11:47:32 -0200

    Listagem de users cadastrados
    Listagem de todos os users cadastrados, dando erro porque nao bate com
    as regras e por faltar coisas no model de User

--- app/src/main/java/com/dubium/fragments/HomeFragment.java
+++ app/src/main/java/com/dubium/fragments/HomeFragment.java
@@ -10,12 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.dubium.R;
 import com.dubium.adapters.UserAdapter;
 import com.dubium.model.User;
+import com.dubium.views.HomeActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +35,10 @@ public class HomeFragment extends Fragment {
     ListView mUsersListView;
     UserAdapter mUserAdapter;
     SwipeRefreshLayout mSwipeRefreshLayout;
+
+    FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mUsersDatabaseReference = mFirebaseDatabase.getReference().child("users");
+
 
     boolean initialRefresh = true;
 
@@ -50,14 +63,18 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        if (initialRefresh == true) {
+        listar();
+        if (initialRefresh == true)
+            initialRefresh = false;
+
+       /* if (initialRefresh == true) {
             users.add(new User("Marcus Vinicius","https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg", 1, 2, 12));
             initialRefresh = false;
         }
 
         mUserAdapter = new UserAdapter(mRootView.getContext(), users);
 
-        mUsersListView.setAdapter(mUserAdapter);
+        mUsersListView.setAdapter(mUserAdapter);*/
 
         return mRootView;
     }
@@ -66,10 +83,40 @@ public class HomeFragment extends Fragment {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                mUserAdapter.add(new User("Marcus Vinicius","https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg", 1, 2, 12));
+                //mUserAdapter.add(new User("Marcus Vinicius","https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg", 1, 2, 12));
                 mSwipeRefreshLayout.setRefreshing(false);
             }
         }, 100);
+    }
+
+    public void listar(){
+        final ArrayList<User> users = new ArrayList<>();
+
+        mUsersDatabaseReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user;
+                users.clear();
+
+                for (DataSnapshot objSnapshot : dataSnapshot.getChildren()) {
+                    user = objSnapshot.getValue(User.class);
+                    users.add(user);
+                }
+                if (users.size() <= 0) {
+                    Toast.makeText(getActivity(), "Não há nenhum pedido",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    UserAdapter adapter = new UserAdapter(getActivity(), users );
+
+                    mUsersListView.setAdapter(adapter);
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {Toast.makeText(getActivity(), "ERRO", Toast.LENGTH_SHORT).show();}
+        });
+
     }
 
 }
